80bab78fcc59e9f5a94d791c32cf6fcae6f449f1
Edvard <edvard.jaegtnes@gmail.com>
2023-04-05 15:33:53 +0200

    Initial commit

--- main.py
+++ main.py
@@ -14,8 +14,33 @@ horizontalMotor: Motor = Motor(Port.C)
 colorSensor: ColorSensor = ColorSensor(Port.S2)
 touchSensor: TouchSensor = TouchSensor(Port.S1)
 
+def resetClaw():
+    verticalMotor.run_target(300,-150)
+    #clawMotor.run_angle(500,20)
+    pass
+
+def interactWithItem():
+
+    clawMotor.run_angle(500,-50)
+    wait(1000)
+    verticalMotor.run_target(300,250)
+    wait(1000)
+    #clawMotor.run_target(500,50, wait=False)    #denna måste vara false när den plockar upp 
+    clawMotor.run_until_stalled
+
+    verticalMotor.run_target(300,-250)
+    
+    
+    #wait(1000)
+    
+    #while True:
+        #clawMotor.run_angle(500,i*10)   #första värde är grader/sekund, andra är hur många grader som öppnas
+        #wait(10)
+
 def main() -> int:
-    #grab
+    #dropItem()
+    resetClaw()
+    interactWithItem()
     return 0
 
 if __name__ == "__main__":
