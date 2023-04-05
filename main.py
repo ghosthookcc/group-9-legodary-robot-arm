00eb7b4730a0d6f696beca897b57ab30f062b880
Emil Andersson <59053660+Xzizide@users.noreply.github.com>
2023-04-05 14:07:01 +0200

    Find Color
    Adds function that returns current color from the color sensor

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
+ev3 = EV3Brick()
+
+colorSensor = ColorSensor(Port.S2)
+
+def findColor():
+    return colorSensor.color()
+
 def main() -> int:
     return 0
 
