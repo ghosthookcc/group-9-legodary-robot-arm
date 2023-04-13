8deeb8a600beb659a8d2f5b44f6d637d1592190f
Adam-Mattsson <adammattsson03@gmail.com>
2023-04-13 15:13:58 +0200

    Implemented DegreeConverstion & Position Movement

--- main.py
+++ main.py
@@ -7,15 +7,30 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-clawMotor: Motor = Motor(Port.A)
-verticalMotor: Motor = Motor(Port.B)
-horizontalMotor: Motor = Motor(Port.C)
+clawMotor = Motor(Port.A)
+verticalMotor = Motor(Port.B)
+horizontalMotor = Motor(Port.C)
 
-colorSensor: ColorSensor = ColorSensor(Port.S2)
-touchSensor: TouchSensor = TouchSensor(Port.S1)
+colorSensor = ColorSensor(Port.S2)
+touchSensor = TouchSensor(Port.S1)
 
-def FindItemAtLocation(position: any):
-    pass
+robotHorizontalMotorAngle = 0
+
+def motorAngleToDegrees(angle: float) -> float:
+    return angle / 4
+
+def degreeToMotorAngle(degree: float) -> float:
+    return degree * 4
+
+def moveToGivenDegree(horizontalPositionDegree: any):
+    global robotHorizontalMotorAngle
+    itemHorizontalMotorAngle = degreeToMotorAngle(horizontalPositionDegree)
+    if itemHorizontalMotorAngle < robotHorizontalMotorAngle:
+        moveAngle = abs(robotHorizontalMotorAngle) + abs(itemHorizontalMotorAngle)
+        horizontalMotor.run_angle(200, (-moveAngle))
+    elif itemHorizontalMotorAngle > robotHorizontalMotorAngle:
+        moveAngle = abs(robotHorizontalMotorAngle) + abs(itemHorizontalMotorAngle)
+        horizontalMotor.run_angle(200, (moveAngle))
 
 def main() -> int:
     return 0
